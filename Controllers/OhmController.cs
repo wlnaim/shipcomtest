018365c14b961c5d474932df85402536cb262022
Ernesto de la Cruz Morales <ernesto.delacruz.morales@gmail.com>
2021-06-14 16:32:43 -0500

    Stub version

---
+++ Controllers/OhmController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace shipcomtest.Controllers
+{
+    [Route("api/[controller]")]
+    public class OhmController : Controller
+    {
+        /// <summary>
+        /// Calculates the Ohm value of a resistor based on the band colors.
+        /// </summary>
+        /// <param name="bandAColor">The color of the first figure of component value band.</param>
+        /// <param name="bandBColor">The color of the second significant figure band.</param>
+        /// <param name="bandCColor">The color of the decimal multiplier band.</param>
+        /// <param name="bandDColor">The color of the tolerance value band.</param>
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public ActionResult GetOhmResult(string bandAColor, string bandBColor, string bandCColor, string bandDColor)
+        {
+            try
+            {
+                return new JsonResult(new { Result = 100 });
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+
+    }
+}
